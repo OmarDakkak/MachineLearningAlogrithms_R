c158cf05e0a12eea494b0b5be58795bf9d8d1fa4
Omar Dakkak <omardakkak98@gmail.com>
2019-10-20 17:38:25 +0100

    MultipleLinearRegression

---
+++ Regression/Multiple Linear Regression/multiple_linear_regression.R
@@ -0,0 +1,34 @@
+# Multiple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+# dataset = dataset[, 2:3]
+
+# Encoding Categorical data 
+
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California','Florida'),
+                       labels = c(1, 2, 3)) 
+
+# Splitting the dataset into the training set a nd the test set
+
+###install.packages('caTools')
+
+library(caTools)
+set.seed(123)
+
+split = sample.split(dataset$Profit, SplitRatio = 0.8) # dataset$DependentVariable 
+
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+# Fitting Multiple Linear Regression to the Training set
+
+#regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State)
+regressor = lm(formula = Profit ~ .,
+               data = training_set) # The "." (dot) replaces all the other independent variables 
+summary(regressor)
