2fa92314e76d3838a5b1e51aa27567912d200bae
Omar Dakkak <omardakkak98@gmail.com>
2019-10-26 15:59:27 +0100

    Polynomial Regression

--- Regression/Polynomial Regression/polynomial_regression.R
+++ Regression/Polynomial Regression/polynomial_regression.R
@@ -25,5 +25,38 @@ dataset = dataset[2:3]
 # Fitting Polynomial Regression to the dataset
   dataset$Level2 = dataset$Level^2;
   dataset$Level3 = dataset$Level^3;
+  dataset$Level4 = dataset$Level^4;
+  #dataset$Level5 = dataset$Level^5;
   poly_reg = lm(formula = Salary ~ .,
                 data = dataset)
+# Visualising the Linear Regression results
+  # install.packages('ggplot2')
+  library(ggplot2)
+  ggplot() +
+    geom_point(aes(x= dataset$Level, y=dataset$Salary),
+               color = 'red') +
+    geom_line(aes(x= dataset$Level, y= predict(lin_reg, newdata = dataset)),
+              color = 'green') +
+    ggtitle('Truth or bluff (Linear Regression)') +
+    xlab('Level') +
+    ylab('Salary')
+  
+  
+# Visualising the Polynomial Regression results
+  
+  ggplot() +
+    geom_point(aes(x= dataset$Level, y=dataset$Salary),
+               color = 'red') +
+    geom_line(aes(x= dataset$Level, y= predict(poly_reg, newdata = dataset)),
+              color = 'green') +
+    ggtitle('Truth or bluff (Polynomial Regression)') +
+    xlab('Level') +
+    ylab('Salary')
+# Predicting a new result with Linear Regression   
+  y_pred = predict(lin_reg, data.frame(Level = 6.5))
+# Predicting a new result with Polynomial Regression     
+  y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                        Level2 = 6.5^2,
+                                        Level3 = 6.5^3,
+                                        Level4 = 6.5^4))
+  
