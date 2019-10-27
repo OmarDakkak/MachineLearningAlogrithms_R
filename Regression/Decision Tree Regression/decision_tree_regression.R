34136040fa034494bdc2457feb5067068b858f6e
Omar Dakkak <omardakkak98@gmail.com>
2019-10-27 17:24:39 +0100

    Decision Tree Regression

---
+++ Regression/Decision Tree Regression/decision_tree_regression.R
@@ -0,0 +1,49 @@
+# Decision Tree Regression 
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into the training set and the test set
+
+###install.packages('caTools')
+# 
+# library(caTools)
+# set.seed(123)
+# 
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8) # dataset$DependentVariable 
+# 
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+# Fitting the Decision Tree Regression to the dataset
+#install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = Salary ~.,
+                  data = dataset, 
+                  control = rpart.control(minsplit = 1))
+
+
+# Predicting a new result     
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the Decision Tree Regression results (higher resolution)
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'green') +
+  ggtitle('Truth or Bluff (Decision Tree Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+# Plotting the tree
+plot(regressor)
+text(regressor)
+
